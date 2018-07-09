ca1bb9a7c7a6c16c47b68b0691db16ad2f5c7610
Fourat Janabi <fouratj@gmail.com>
2018-07-09 20:29:05 -0300

    Code style improvement

--- map.js
+++ map.js
@@ -3,6 +3,6 @@ module.exports = (arr, cb) => {
     throw new Error('First argument must be of type Array');
     return;
   }
-  
+
   return arr.reduce((pre, curr) => [...pre, cb(curr)], []);
-}
+};
